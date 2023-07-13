bfd03bc108d5268ea6eb2840aac1114aa40f2fe5
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-07-13 20:10:38 +0600

    perform post and get method

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express=require('express');
-const app=express();
-const port=process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 require('dotenv').config()
-const cors=require('cors')
+const cors = require('cors')
 // user-management
 // oGwEm3VmYTVnlug2
 
@@ -17,34 +17,62 @@ const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Password}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const userCollection = client.db('userManagementDB').collection('users');
+
+        // users
+
+        // Post Method
+
+        app.post('/postUser',async (req, res) => {
+            const userData = req.body;
+            if (!userData) {
+                return res.status(404).send({ message: "userData not Found" })
+            }
+            const result= await userCollection.insertOne(userData);
+            res.send(result);
+        })
+
+        // get method
+      app.get('/users',async (req,res)=>{
+       
+        const result=await userCollection.find().toArray();
+        res.send(result);
+
+      })
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('user management system server')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
