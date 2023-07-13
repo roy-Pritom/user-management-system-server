833d2e03adfb99277415b700e0661a2c43010c8c
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-07-13 19:46:09 +0600

    create express js server and connect to  Mongodb

---
+++ index.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const port=process.env.PORT || 3000;
+require('dotenv').config()
+const cors=require('cors')
+// user-management
+// oGwEm3VmYTVnlug2
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.User_Name}:${process.env.User_Password}@cluster0.ow3zltf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('user management system server')
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
